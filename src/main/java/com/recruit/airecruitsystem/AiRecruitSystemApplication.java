f1680da28c6fe805517c32d76264ba01ba29572a
koi <1763620981@qq.com>
2026-05-12 16:35:35 +0800

    feat:完成后端基础框架搭建

---
+++ src/main/java/com/recruit/airecruitsystem/AiRecruitSystemApplication.java
@@ -0,0 +1,13 @@
+package com.recruit.airecruitsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiRecruitSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiRecruitSystemApplication.class, args);
+    }
+
+}
