44ad8c292247ba8a587162bb052b185db93e50a5
wumeng <1838375352@qq.com>
2026-06-23 15:34:26 +0800

    对发送面试邀请手机号的修改

--- src/main/java/com/recruit/airecruitsystem/dto/hr/SendInterviewRequest.java
+++ src/main/java/com/recruit/airecruitsystem/dto/hr/SendInterviewRequest.java
@@ -3,6 +3,7 @@ package com.recruit.airecruitsystem.dto.hr;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -24,11 +25,11 @@ public class SendInterviewRequest {
 
     @NotBlank(message = "面试形式不能为空")
     @JsonProperty("interview_type")
-    private String interviewType;      // 线上面试 / 现场面试 / 电话面试
+    private String interviewType;
 
     @NotBlank(message = "面试轮次不能为空")
     @JsonProperty("interview_round")
-    private String interviewRound;     // 初试 / 复试 / 终试
+    private String interviewRound;
 
     @NotBlank(message = "面试地点不能为空")
     @JsonProperty("interview_address")
@@ -38,7 +39,8 @@ public class SendInterviewRequest {
     @JsonProperty("contact_name")
     private String contactName;
 
-    @NotBlank(message = "联系人电话不能为空")
+    @NotBlank(message = "联系电话不能为空")
+    @Pattern(regexp = "^1[3-9]\\d{9}$", message = "手机号格式不正确")
     @JsonProperty("contact_phone")
     private String contactPhone;
 
